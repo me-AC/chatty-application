774bfeddad629eb6d1caefd2bf6919154e0fddb0
Abhinav <abhinavchaudhary2014@gmail.com>
2025-02-18 01:40:53 +0530

    fix: overflow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,66 @@
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 
-import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import SettingsPage from "./pages/SettingsPage";
-import ProfilePage from "./pages/ProfilePage";
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
 
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthStore } from "./store/useAuthStore";
-import { useThemeStore } from "./store/useThemeStore";
-import { useEffect } from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
+import { useEffect } from 'react';
 
-import { Loader } from "lucide-react";
-import { Toaster } from "react-hot-toast";
+import { Loader } from 'lucide-react';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
-  const { theme } = useThemeStore();
+	const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+	const { theme } = useThemeStore();
 
-  console.log({ onlineUsers });
+	console.log(theme);
 
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
+	console.log({ onlineUsers });
 
-  console.log({ authUser });
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
 
-  if (isCheckingAuth && !authUser)
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-      </div>
-    );
+	console.log({ authUser });
 
-  return (
-    <div data-theme={theme}>
-      <Navbar />
+	if (isCheckingAuth && !authUser)
+		return (
+			<div className="flex items-center justify-center">
+				<Loader className="size-10 animate-spin" />
+			</div>
+		);
 
-      <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-      </Routes>
+	return (
+		<div data-theme={theme} className="overflow-auto">
+			<Navbar />
 
-      <Toaster />
-    </div>
-  );
+			<Routes>
+				<Route
+					path="/"
+					element={authUser ? <HomePage /> : <Navigate to="/login" />}
+				/>
+				<Route
+					path="/signup"
+					element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/login"
+					element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+				/>
+				<Route path="/settings" element={<SettingsPage />} />
+				<Route
+					path="/profile"
+					element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+				/>
+			</Routes>
+
+			<Toaster />
+		</div>
+	);
 };
 export default App;
